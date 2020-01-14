dc8cc4fd13286effa483561d2d08e95860fa8144
Corbin Hughes <cshughes97@yahoo.com>
2020-01-14 12:08:56 -0700

    added color changing

--- web/teach02/clickMe.js
+++ web/teach02/clickMe.js
@@ -2,3 +2,9 @@ function clicked() {
 	console.log("Button was clicked.");
 	window.alert("Clicked!");
 }
+
+function changeColor() {
+	console.log("Time to change");
+	var color = document.getElementById("color").innerHTML;
+	document.getElementById("changeable").style.color = color;
+}
