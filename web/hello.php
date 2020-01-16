d033691b09acbb407a8ea46a85633cdb085988d2
Corbin Hughes <cshughes97@yahoo.com>
2020-01-15 20:04:20 -0700

    OK I think we got it

---
+++ web/hello.php
@@ -0,0 +1,10 @@
+<!doctype html> 
+<html lang="en-US" style="height:100%">
+<head>
+<link rel="stylesheet" type="text/css" href="hello.css">
+</head>
+<body>
+	<h1>Hello World!</h1>
+	<?php include "main.php"; ?>
+</body>
+</html>
