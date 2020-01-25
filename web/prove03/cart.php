533c626a8dd2bf9fabdc36d8c6ce2314fca30f33
Corbin Hughes <cshughes97@yahoo.com>
2020-01-25 13:33:11 -0700

    fixed some junk with php

--- web/prove03/cart.php
+++ web/prove03/cart.php
@@ -20,38 +20,38 @@
 	<h3>Your order:</h3>
 	<?php $list = explode(",", $_POST["order"]);
 	$total = 0.00;
-	foreach ($order as $item) {
+	foreach ($list as $item) {
 		switch($item) {
 			case "samurai":
-				echo "<p>$24.99 - Samurai Sword</p>";
+				echo "<p>\$24.99 - Samurai Sword</p>";
 				$total += 24.99;
 				break;
 			case "spartan":
-				echo "<p>$29.99 - Spartan Gladius</p>";
+				echo "<p>\$29.99 - Spartan Gladius</p>";
 				$total += 29.99;
 				break;
 			case "knight":
-				echo "<p>$34.99 - Knight\'s Longsword</p>";
+				echo "<p>\$34.99 - Knight\'s Longsword</p>";
 				$total += 34.99;
 				break;
 			case "fencing":
-				echo "<p>$22.99 - Fencing Rapier</p>";
+				echo "<p>\$22.99 - Fencing Rapier</p>";
 				$total += 22.99;
 				break;
 			case "viking":
-				echo "<p>$26.99 - Viking Waraxe</p>";
+				echo "<p>\$26.99 - Viking Waraxe</p>";
 				$total += 26.99;
 				break;	
 			case "compound":
-				echo "<p>$32.99 - Compound Bow</p>";
+				echo "<p>\$32.99 - Compound Bow</p>";
 				$total += 32.99;
 				break;
 			case "battleaxe":
-				echo "<p>$25.99 - Battleaxe</p>";
+				echo "<p>\$25.99 - Battleaxe</p>";
 				$total += 25.99;
 				break;
 			case "longbow":
-				echo "<p>$28.99 - Traditional Longbow</p>";
+				echo "<p>\$28.99 - Traditional Longbow</p>";
 				$total += 28.99;
 				break;
 		}
