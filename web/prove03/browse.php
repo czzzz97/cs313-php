00614dac161b51cbae5677a0c169ee897708a0e8
Corbin Hughes <cshughes97@yahoo.com>
2020-01-25 09:58:00 -0700

    tweaked display and added more gallery items

--- web/prove03/browse.php
+++ web/prove03/browse.php
@@ -5,19 +5,39 @@
 <link rel="stylesheet" type="text/css" href="prove03.css">
 </head>
 <body>
-// <p id=""><?php include '.php';?></p>
-
 <header>
 	<h1>Replica Weapons</h1>
 	<h2>for stage, practice, or display</h2>
 </header>
 
+<div class="container">
 <div class="gallery">
-    <img src="https://cdn11.bigcommerce.com/s-sd2rb2jpmz/images/stencil/1280x1280/products/2789/4008/HK-079-500__00426.1485291127.jpg?c=2&imbypass=on" alt="samurai" width="600" height="400">
-  <div class="desc"></div>
-  <div class="price">$40
+    <img src="https://cdn11.bigcommerce.com/s-sd2rb2jpmz/images/stencil/1280x1280/products/2789/4008/HK-079-500__00426.1485291127.jpg" alt="samurai" width="600" height="400">
+  <div class="desc">Samurai Sword</div>
+  
+  <div class="price">$24.99
   <button id="addSamurai"><span class="fas fa-cart-plus"></span></button>
   </div>
+  
+</div>
+
+<div class="gallery">
+    <img src="https://www.japaneseswords4samurai.com/wp-content/uploads/2018/04/SW-1022.jpg" alt="spartan" width="600" height="400">
+  <div class="desc">Spartan Gladius</div>
+  
+  <div class="price">$29.99
+  <button id="addSpartan"><span class="fas fa-cart-plus"></span></button>
+  </div>
+  
+<div class="gallery">
+    <img src="https://swordskingdom.com/media/catalog/product/cache/1/thumbnail/1000x/17f82f742ffe127f42dca9de82fb58b1/b/l/black-glamdring-sword-of-gandalf-replica-from-lotr.jpg" alt="knight" width="600" height="400">
+  <div class="desc">Knight's Longsword</div>
+  
+  <div class="price">$34.99
+  <button id="addKnight"><span class="fas fa-cart-plus"></span></button>
+  </div>  
+  
+</div>
 </div>
 
 
