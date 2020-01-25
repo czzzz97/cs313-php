df212391132e660a274aa9ad23d5f626dd9e8d53
Corbin Hughes <cshughes97@yahoo.com>
2020-01-25 13:13:47 -0700

    moving orders 2x

--- web/prove03/cart.php
+++ web/prove03/cart.php
@@ -8,34 +8,54 @@
 <header>
 	<div class="nav">
 	<form action="checkout.php" method="post">
-	<input id=orderCart name="order"><?php echo $_POST["order"];?></input>
+	<input id=orderCart name="order"><p><?php print $_POST["order"];?></p></input>
 	<button type="submit">Proceed to checkout</button>
 	</form>
-	</div>
-	<div class="nav">
 	<button onclick="location.href='browse.php'" type="button">Cancel order and browse</button>
 	</div>
 	<h1>Replica Weapons</h1>
 	<h2>for stage, practice, or display</h2>
 </header>
 	<h3>Your order:</h3>
-	<?php echo  $_POST["order"];?> 
-	<table id="cart">
-		<tr>
-			<td>Price</td>
-			<td>Item</td>
-		</tr>
-		<tr>
-			<td>$9.99</td>
-			<td>Example Item</td>
-		</tr>
-		<tr><td></td><td></td></tr>
-		<tr>
-			<td>$20.44</td>
-			<td>total</td>
-		</tr>
-	</table>
-
+	<?php $list = explode(",", $_POST["order"];
+	$total = 0.00;
+	foreach ($order as $item) {
+		switch($item) {
+			case "samurai":
+				echo "<p>$24.99 - Samurai Sword</p>";
+				$total += 24.99;
+				break;
+			case "spartan":
+				echo "<p>$29.99 - Spartan Gladius</p>";
+				$total += 29.99;
+				break;
+			case "knight":
+				echo "<p>$34.99 - Knight\'s Longsword</p>";
+				$total += 34.99;
+				break;
+			case "fencing":
+				echo "<p>$22.99 - Fencing Rapier</p>";
+				$total += 22.99;
+				break;
+			case "viking":
+				echo "<p>$26.99 - Viking Waraxe</p>";
+				$total += 26.99;
+				break;	
+			case "compound":
+				echo "<p>$32.99 - Compound Bow</p>";
+				$total += 32.99;
+				break;
+			case "battleaxe":
+				echo "<p>$25.99 - Battleaxe</p>";
+				$total += 25.99;
+				break;
+			case "longbow":
+				echo "<p>$28.99 - Traditional Longbow</p>";
+				$total += 28.99;
+				break;
+		}
+	}
+	echo "<p>" . $total . "</p>";?> 
 
 </body>
 </html>
