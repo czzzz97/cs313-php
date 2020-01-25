361a8653b727413c8eb2e2b1f17e96e07a2fa1e8
Corbin Hughes <cshughes97@yahoo.com>
2020-01-25 09:23:16 -0700

    added basic structure and first gallery item

---
+++ web/prove03/browse.php
@@ -0,0 +1,25 @@
+<!doctype html> 
+<html lang="en-US" style="height:100%">
+<head>
+<script src="https://kit.fontawesome.com/a23918c4fb.js" crossorigin="anonymous"></script>
+<link rel="stylesheet" type="text/css" href="prove03.css">
+</head>
+<body>
+// <p id=""><?php include '.php';?></p>
+
+<header>
+	<h1>Replica Weapons</h1>
+	<h2>for stage, practice, or display</h2>
+</header>
+
+<div class="gallery">
+    <img src="https://cdn11.bigcommerce.com/s-sd2rb2jpmz/images/stencil/1280x1280/products/2789/4008/HK-079-500__00426.1485291127.jpg?c=2&imbypass=on" alt="samurai" width="600" height="400">
+  <div class="desc"></div>
+  <div class="price">$40
+  <button id="addSamurai"><span class="fas fa-cart-plus"></span></button>
+  </div>
+</div>
+
+
+</body>
+</html>
