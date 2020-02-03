4511cbda2d891528554892118b5b1ecf7653dd74
Corbin Hughes <cshughes97@yahoo.com>
2020-02-03 13:43:24 -0700

    testing queries to db

--- web/test.php
+++ web/test.php
@@ -22,8 +22,14 @@ catch (PDOException $ex)
 
 foreach ($db->query('SELECT title, price FROM game') as $row)
 {
-  echo 'title: ' . $row['title'];
-  echo ' game: ' . $row['game'];
+  echo $row['title'] . ' costs ';
+  echo $row['price'];
   echo '<br/>';
 }
+echo '<br/>';
+$stmt = $db->prepare('SELECT * FROM author WHERE id=:id AND name=:name');
+$stmt->bindValue(':id', $id, PDO::PARAM_INT);
+$stmt->bindValue(':name', $name, PDO::PARAM_STR);
+$stmt->execute();
+$rows = $stmt->fetchAll(PDO::FETCH_ASSOC);
 ?>
