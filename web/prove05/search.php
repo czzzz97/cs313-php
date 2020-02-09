6e394643569d0bf04561e8ccf8deff2df27abc9f
Corbin Hughes <cshughes97@yahoo.com>
2020-02-08 19:13:07 -0700

    tweak 03

--- web/prove05/search.php
+++ web/prove05/search.php
@@ -61,7 +61,7 @@ foreach ($db->query('SELECT title, price FROM game') as $row)
 if ($_POST['searchBy'] = 'author'){
 foreach ($db->query('SELECT game.price, game.title, author.name FROM game INNER JOIN author ON
 game.author_id = author.author_id WHERE author.name = \'' . $_POST['search'] . '\'') as $row) {
-  echo $row['price'] . ' | ' . $row['title'] . ' - by ' . $row['author.name'];
+  echo $row['game.price'] . ' | ' . $row['game.title'] . ' - by ' . $row['author.name'];
   echo '<br/>';
 }}
 else if ($_POST['searchBy'] = 'title'){
